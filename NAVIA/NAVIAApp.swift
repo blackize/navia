b01c3f549a3e1eb2cbad813680d42cb292d57045
blackice <yaremchukvk@gmail.com>
2026-06-06 11:22:32 +0200

    Initial Commit

---
+++ NAVIA/NAVIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NAVIAApp.swift
+//  NAVIA
+//
+//  Created by Viktor on 06.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct NAVIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
