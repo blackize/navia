1726eb3a951f877ad5bdd9ffe9216f2a6b907107
blackice <yaremchukvk@gmail.com>
2026-06-07 08:38:36 +0200

    Connect HomeViewModel to GitHub content API

--- NAVIA/ViewModels/HomeViewModel.swift
+++ NAVIA/ViewModels/HomeViewModel.swift
@@ -6,20 +6,36 @@ class HomeViewModel: ObservableObject {
     @Published var recommendedPath: CareerPath?
     @Published var alternativePaths: [CareerPath] = []
     @Published var lastWatchedLesson: Lesson?
+    @Published var isLoading = false
     
     init() {
-        loadData()
+        loadCourses()
     }
     
-    private func loadData() {
-        let paths = CourseService.shared.getCoursePaths()
-        recommendedPath = paths.first
-        alternativePaths = Array(paths.dropFirst().prefix(2))
-        
-        if let firstPath = recommendedPath,
-           let firstModule = firstPath.modules.first,
-           let firstLesson = firstModule.lessons.first {
-            lastWatchedLesson = firstLesson
+    private func loadCourses() {
+        Task {
+            isLoading = true
+            do {
+                let paths = try await CourseService.shared.fetchCoursesFromGitHub()
+                await MainActor.run {
+                    recommendedPath = paths.first
+                    alternativePaths = Array(paths.dropFirst().prefix(2))
+                    
+                    if let firstPath = recommendedPath,
+                       let firstModule = firstPath.modules.first,
+                       let firstLesson = firstModule.lessons.first {
+                        lastWatchedLesson = firstLesson
+                    }
+                }
+            } catch {
+                print("Failed to load courses: \(error)")
+                await MainActor.run {
+                    let paths = CourseService.shared.getCoursePaths()
+                    recommendedPath = paths.first
+                    alternativePaths = Array(paths.dropFirst().prefix(2))
+                }
+            }
+            isLoading = false
         }
     }
 }
